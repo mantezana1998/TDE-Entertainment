7f342823bb77699f3f2200149cfe3aedc91a8da0
mantezana <maximo00antezana@gmail.com>
2022-01-28 13:37:45 -0800

    layout and outlet working

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Grid, Box } from 'grommet';
+import { Outlet } from 'react-router-dom'
 
 export default function Layout (){
     return(
-        <>
-            <div style={{ display: "grid", gridTemplateColumns: "1fr 2fr"}}>
-                <div 
-                style={{
-                    borderRight: '2px solid black', 
-                    height: '100vh', 
-                    margin: 0
-                    }}>
-                        <img src="https://i.imgur.com/2by6cBW.jpg" height='200px' width='400px' style={{marginTop: '40%'}}/>
-                </div>
-            </div>
-        </>
+        <Grid
+            areas={[
+            { name: 'side', start: [0, 0], end: [0, 0] },
+            { name: 'main', start: [1, 0], end: [2, 0] },
+            ]}
+            columns={['medium', 'large']}
+            rows={['large']}
+            gap='xxsmall'
+            >
+            
+            <Box gridArea='side' background='red' >
+                <Outlet />
+            </Box>
+            <Box gridArea='main' background='blue'>
+                <Outlet />
+            </Box>
+        </Grid>
     )
 }
