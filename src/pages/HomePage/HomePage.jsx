559999c1840bddf1099b04e0f4190c7434a3a35c
mantezana <maximo00antezana@gmail.com>
2022-01-28 15:40:11 -0800

    artist flexbox photo layout complete

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import './HomePage.css'
 
 export default function HomePage(){
     return(
-        <h1>hi</h1>
-        // <div>
-        // <img src="https://i.imgur.com/UX6tVOl.jpg" alt="Isaiah Rashad" height='300px' width='500px'/>
-        // <img src="https://i.imgur.com/Xdaojjg.jpg" alt="Ab-Soul" height='300px' width='500px'/>
-        // <img src="https://i.imgur.com/lCjUbRL.jpg" alt="Reason" height='300px' width='500px'/>
-        // <img src="https://i.imgur.com/e1LznTs.jpg" alt="SZA" height='300px' width='500px'/>
-        // <img src="https://i.imgur.com/tcAr2eS.jpg" alt="Kendrick Lamar" height='300px' width='500px'/>
-        // <img src="https://i.imgur.com/6Cn0f0r.jpg" alt="Schoolboy Q" height='300px' width='500px'/>
-        // <img src="https://i.imgur.com/EDlDYwH.jpg" alt="Lance Skiiwalker" height='300px' width='500px'/>
-        // <img src="https://i.imgur.com/tqNwdKR.jpg" alt="Zacari" height='300px' width='500px'/>
-        // <img src="https://i.imgur.com/vTsNt9d.jpg" alt="SiR" height='300px' width='500px'/>
-        // </div>
+        <div className='row'>
+            <div className='column'>
+                <img src="https://i.imgur.com/e1LznTs.jpg" alt="SZA" height='300px' width='500px'/>
+                <img src="https://i.imgur.com/UX6tVOl.jpg" alt="Isaiah Rashad" height='300px' width='500px'/>
+            </div>
+            <div className='column'>
+                <img src="https://i.imgur.com/Xdaojjg.jpg" alt="Ab-Soul" height='300px' width='500px'/>
+                <img src="https://i.imgur.com/tcAr2eS.jpg" alt="Kendrick Lamar" height='200px' width='500px'/>
+            </div>
+            <div className="column">
+                <img src="https://i.imgur.com/6Cn0f0r.jpg" alt="Schoolboy Q" height='300px' width='500px'/>
+                <img src="https://i.imgur.com/EDlDYwH.jpg" alt="Lance Skiiwalker" height='400px' width='500px'/>
+            </div>
+            <div className="column">
+                <img src="https://i.imgur.com/tqNwdKR.jpg" alt="Zacari" height='350px' width='500px'/>
+                <img src="https://i.imgur.com/vTsNt9d.jpg" alt="SiR" height='300px' width='500px'/>
+                <img src="https://i.imgur.com/lCjUbRL.jpg" alt="Reason" height='400px' width='500px'/>
+            </div>
+        </div>
     )
 }
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -7,15 +7,15 @@ export default function Layout (){
         <Grid
             areas={[
             { name: 'side', start: [0, 0], end: [0, 0] },
-            { name: 'main', start: [1, 0], end: [2, 0] },
+            { name: 'main', start: [1, 0], end: [3, 0] },
             ]}
             columns={['medium', 'large']}
             rows={['large']}
             gap='xxsmall'
             >
             
-            <Box gridArea='side' background='red' >
-                <Outlet />
+            <Box gridArea='side'>
+                <img src="https://i.imgur.com/2by6cBW.jpg" alt="TDE Logo" style={{marginTop : '20%'}}/>
             </Box>
             <Box gridArea='main' background='blue'>
                 <Outlet />
