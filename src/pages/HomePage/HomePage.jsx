fba9ac9cc45173fec52bbff332df16311e0c8f0d
mantezana <maximo00antezana@gmail.com>
2022-01-28 17:21:56 -0800

    artist picture grid filled in

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,20 +6,20 @@ export default function HomePage(){
         <div className='row'>
             <div className='column'>
                 <img src="https://i.imgur.com/e1LznTs.jpg" alt="SZA" height='300px' width='500px'/>
-                <img src="https://i.imgur.com/UX6tVOl.jpg" alt="Isaiah Rashad" height='300px' width='500px'/>
+                <img src="https://i.imgur.com/lCjUbRL.jpg" alt="Reason" height='400px' width='500px'/>
             </div>
             <div className='column'>
-                <img src="https://i.imgur.com/Xdaojjg.jpg" alt="Ab-Soul" height='300px' width='500px'/>
-                <img src="https://i.imgur.com/tcAr2eS.jpg" alt="Kendrick Lamar" height='200px' width='500px'/>
+                <img src="https://i.imgur.com/UX6tVOl.jpg" alt="Isaiah Rashad" height='300px' width='500px'/>
+                <img src="https://i.imgur.com/p74Ib0Bl.jpg" alt="Kendrick Lamar" height='400px' width='500px'/>
             </div>
             <div className="column">
-                <img src="https://i.imgur.com/6Cn0f0r.jpg" alt="Schoolboy Q" height='300px' width='500px'/>
+                <img src="https://i.imgur.com/6Cn0f0r.jpg" alt="Schoolboy Q" height='400px' width='600px'/>
                 <img src="https://i.imgur.com/EDlDYwH.jpg" alt="Lance Skiiwalker" height='400px' width='500px'/>
             </div>
             <div className="column">
-                <img src="https://i.imgur.com/tqNwdKR.jpg" alt="Zacari" height='350px' width='500px'/>
+                <img src="https://i.imgur.com/Xdaojjg.jpg" alt="Ab-Soul" height='300px' width='500px'/>
                 <img src="https://i.imgur.com/vTsNt9d.jpg" alt="SiR" height='300px' width='500px'/>
-                <img src="https://i.imgur.com/lCjUbRL.jpg" alt="Reason" height='400px' width='500px'/>
+                <img src="https://i.imgur.com/2by6cBW.jpg" alt="TDE Logo"/>
             </div>
         </div>
     )
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Grid, Box } from 'grommet';
 import { Outlet } from 'react-router-dom'
+import './Layout.css'
 
 export default function Layout (){
     return(
         <Grid
             areas={[
-            { name: 'side', start: [0, 0], end: [0, 0] },
+            { name: 'side', start: [0, 0], end: [1, 0] },
             { name: 'main', start: [1, 0], end: [3, 0] },
             ]}
             columns={['medium', 'large']}
-            rows={['large']}
+            rows={['xxxsmall']}
             gap='xxsmall'
             >
             
-            <Box gridArea='side'>
-                <img src="https://i.imgur.com/2by6cBW.jpg" alt="TDE Logo" style={{marginTop : '20%'}}/>
+            <Box gridArea='side' className='side'>
+                <img src="https://i.imgur.com/2by6cBW.jpg" alt="TDE Logo"/>
             </Box>
-            <Box gridArea='main' background='blue'>
+            <Box gridArea='main' className='main'>
                 <Outlet />
             </Box>
         </Grid>
